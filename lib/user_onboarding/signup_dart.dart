029c7c14cafd286ed21c11bf24b065c4ac7ff444
Aaditya Jagdale <81344234+Contro1-cs@users.noreply.github.com>
2023-07-02 11:41:51 +0530

    basic changes

--- lib/user_onboarding/signup_dart.dart
+++ lib/user_onboarding/signup_dart.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:unaccountable/landing_page.dart';
@@ -21,125 +23,138 @@ class _SignupPageState extends State<SignupPage> {
     var w = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: backgroundgreen,
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-          toolbarHeight: 150,
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: Text(
-            'Get your ass up and start\nworking motherfucker!',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(
-              fontSize: 18,
-              color: Colors.black,
-            ),
-          )),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(height: 1),
-          Column(
+        backgroundColor: backgroundgreen,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              SignUpFormField(
-                controller: emailController,
-                title: 'Email',
-                hint: 'aaditya@gmail.com',
-                inputType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 20),
               Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 35),
-                    child: Text(
-                      'Password',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  const SizedBox(height: 50),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
                     ),
                   ),
-                  Container(
-                    width: w,
-                    margin: const EdgeInsets.symmetric(horizontal: 25),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: TextField(
-                      obscureText: _hidePassword,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Minimum 6 characters',
-                          hintStyle: GoogleFonts.poppins(color: Colors.grey),
-                          suffixIcon: _hidePassword
-                              ? IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _hidePassword = !_hidePassword;
-                                    });
-                                  },
-                                  icon: const Icon(
-                                    Icons.visibility,
-                                    color: Colors.white,
-                                  ))
-                              : IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _hidePassword = !_hidePassword;
-                                    });
-                                  },
-                                  icon: Icon(
-                                    Icons.visibility_off,
-                                    color: Colors.white.withOpacity(0.8),
-                                  ),
-                                )),
-                      cursorColor: Colors.white,
-                      controller: passwordController,
-                      style: GoogleFonts.poppins(color: Colors.white),
-                      keyboardType: TextInputType.visiblePassword,
+                  const SizedBox(height: 15),
+                  Text(
+                    'Get off that bed fucker!',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
                   )
                 ],
               ),
+              const SizedBox(height: 120),
+              Column(
+                children: [
+                  SignUpFormField(
+                    controller: emailController,
+                    title: 'Email',
+                    hint: 'aaditya@gmail.com',
+                    inputType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 35),
+                        child: Text(
+                          'Password',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: w,
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: TextField(
+                          obscureText: _hidePassword,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Minimum 6 characters',
+                              hintStyle:
+                                  GoogleFonts.poppins(color: Colors.grey),
+                              suffixIcon: _hidePassword
+                                  ? IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _hidePassword = !_hidePassword;
+                                        });
+                                      },
+                                      icon: const Icon(
+                                        Icons.visibility,
+                                        color: Colors.white,
+                                      ))
+                                  : IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _hidePassword = !_hidePassword;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.visibility_off,
+                                        color: Colors.white.withOpacity(0.8),
+                                      ),
+                                    )),
+                          cursorColor: Colors.white,
+                          controller: passwordController,
+                          style: GoogleFonts.poppins(color: Colors.white),
+                          keyboardType: TextInputType.visiblePassword,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 120),
+              Container(
+                height: 50,
+                width: w,
+                margin: const EdgeInsets.fromLTRB(25, 0, 25, 30),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const UserInfo(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign up',
+                    style: GoogleFonts.inter(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
             ],
           ),
-          Container(
-            height: 50,
-            width: w,
-            margin: const EdgeInsets.fromLTRB(25, 0, 25, 30),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const UserInfo(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                shadowColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              child: Text(
-                'Sign up',
-                style: GoogleFonts.inter(
-                    color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
--- lib/user_onboarding/user_info.dart
+++ lib/user_onboarding/user_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:unaccountable/home/home.dart';
 import 'package:unaccountable/landing_page.dart';
 import 'package:unaccountable/lists/lists.dart';
 import 'package:unaccountable/widgets/custom_form_field.dart';
@@ -22,93 +23,143 @@ class _UserInfoState extends State<UserInfo> {
   Widget build(BuildContext context) {
     var w = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      backgroundColor: backgroundgreen,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text(
-          'Who the fuck are you?',
-          style: GoogleFonts.poppins(
-            fontWeight: FontWeight.bold,
+    return WillPopScope(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: backgroundgreen,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text(
+            'Who the fuck are you?',
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 10),
+                  UserInfoFormField(
+                    controller: name,
+                    title: 'Name',
+                    hint: 'Aaditya Jagdale',
+                    inputType: TextInputType.name,
+                  ),
+                  const SizedBox(height: 10),
+                  UserInfoFormField(
+                    controller: age,
+                    title: 'Age',
+                    hint: '21',
+                    inputType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 10),
+                  CustomDropdownButton(
+                    title: 'Gender',
+                    list: genders,
+                    displayedValue: _gender,
+                    onChanged: (value) {
+                      setState(() {
+                        _gender = value;
+                      });
+                    },
+                  ),
+                  CustomDropdownButton(
+                    title: 'Country',
+                    list: countries,
+                    displayedValue: _country,
+                    onChanged: (value) {
+                      setState(() {
+                        _country = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                height: 50,
+                width: w,
+                margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign up',
+                    style: GoogleFonts.inter(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 10),
-                UserInfoFormField(
-                  controller: name,
-                  title: 'Name',
-                  hint: 'Aaditya Jagdale',
-                  inputType: TextInputType.name,
-                ),
-                const SizedBox(height: 10),
-                UserInfoFormField(
-                  controller: name,
-                  title: 'Age',
-                  hint: '21',
-                  inputType: TextInputType.number,
-                ),
-                const SizedBox(height: 10),
-                CustomDropdownButton(
-                  title: 'Gender',
-                  list: genders,
-                  displayedValue: _gender,
-                  onChanged: (value) {
-                    setState(() {
-                      _gender = value;
-                    });
-                  },
-                ),
-                CustomDropdownButton(
-                  title: 'Country',
-                  list: countries,
-                  displayedValue: _country,
-                  onChanged: (value) {
-                    setState(() {
-                      _country = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            Container(
-              height: 50,
-              width: w,
-              margin: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const UserInfo(),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shadowColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-                child: Text(
-                  'Sign up',
-                  style: GoogleFonts.inter(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+      onWillPop: () async {
+        final shouldPop = await showDialog<bool>(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              title: Text(
+                "Don't exit the app without filling the information",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  color: Colors.black,
                 ),
               ),
-            )
-          ],
-        ),
-      ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              actionsAlignment: MainAxisAlignment.spaceEvenly,
+              actions: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: Container(
+                    height: 50,
+                    width: w,
+                    decoration: BoxDecoration(
+                      color: backgroundgreen,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Okay",
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+        return shouldPop!;
+      },
     );
   }
 }
